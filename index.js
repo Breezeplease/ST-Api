a91e5e16ab6d04c947de7cf16872f66986c240c1
Clayton Campbell <breezeplz@gmail.com>
2018-01-17 16:58:48 -0500

    api walkthrough

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 $(() => {
+  $(`#infoButton`).click(() => {
+    $.ajax({
+      type: 'GET',
+      url: 'https://swapi.co/api/people/'
+    }).done((res) => { //res = keyword    change it to whatever
+      let data = res.results
+      for(person of data) {
+        console.log(person.name)
+      }
+    })
+  })
 
 })
 // short for $(document).ready(function) {}
+
