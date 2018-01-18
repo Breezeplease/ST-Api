c0a90d96b4e5920cc7d44c31dee8d5d01013a49d
Clayton Campbell <breezeplz@gmail.com>
2018-01-18 10:40:43 -0500

    api walkthrough

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+let loaded = false
+
 $(() => {
   $(`#infoButton`).click(() => {
-    $.ajax({
-      type: 'GET',
-      url: 'https://swapi.co/api/people/'
-    }).done((res) => { //res = keyword    change it to whatever
-      let data = res.results
-      for(person of data) {
-        console.log(person.name)
-      }
-    })
+    if(!loaded){
+      $.ajax({
+       type: 'GET',
+        url: 'https://swapi.co/api/people/'
+     }).done((res) => { //res = keyword    change it to whatever
+        let data = res.results
+        for(person of data) {
+         // tableBody.append(person)
+          $('#tableBody').append(createTableRow(person))
+        }
+        loaded = true
+      })
+    }
   })
 
+function createTableRow(person) {
+  let row = $(`<tr></tr>`)
+  let name = $(`<td>${person.name}</td>`)
+  let height = $(`<td>${person.height}</td>`)
+  let birth = $(`<td>${person.birth_year}</td>`)
+
+  name.css('color','red')
+
+  row.append(name)
+  row.append(height)
+  row.append(birth)
+
+  return row
+}
+
 })
-// short for $(document).ready(function) {}
+// $(() => {}) short for $(document.ready(function) {})
 
