3428279d8f8faa262b5041b29a0464c306b2abe0
Clayton Campbell <breezeplz@gmail.com>
2018-01-24 18:47:22 -0500

    API Finish

--- index.js
+++ index.js
@@ -1,112 +1,94 @@
-let loaded = false
 
 $(() => {
+localStorage.clear()
+let storedStudents = []
+let storedStaff = []
+//   let myData = {
+//   title: 'Information',
+//   content:  [1,2,4,4,5,9] 
+// }
+
+// localStorage.setItem('setting',myData)
+
   $(`#infoButton`).click(() => {
-    if(!loaded){
-      loaded = true
+    $('#tableBody').empty()
+    $('#frontOne').text('Name')
+    $('#frontTwo').text('Gender')
+    $('#frontThree').text('House')
+
+    if(!localStorage.getItem('storedStudents')){
       $.ajax({
        type: 'GET',
-        url: 'https://swapi.co/api/people/'
+        url: 'http://hp-api.herokuapp.com/api/characters/students'
      }).done((res) => { //res = keyword    change it to whatever
-        $('#frontOne').text('Name')
-        $('#frontTwo').text('Height')
-        $('#frontThree').text('Birth Year')
-      let data = res.results
-        
-        for(person of data) {
-         // tableBody.append(person)
-          $('#tableBody').append(createTableRowOne(person))
+        console.log(res)
+        for(students of res) {
+         // tableBody.append(students)
+         storedStudents.push({name: students.name, gender: students.gender, house: students.house})
+          $('#tableBody').append(createTableRowOne(students))
         }
+        localStorage.setItem('storedStudents', JSON.stringify(storedStudents))
       })
+    } else {
+      for(students of storedStudents) {
+        $('#tableBody').append(createTableRowOne(students))
+      }
     }
   })
   
-  function createTableRowOne(person) {
+  function createTableRowOne(students) {
     let row = $(`<tr></tr>`)
-    let name = $(`<td>${person.name}</td>`)
-    let height = $(`<td>${person.height}</td>`)
-    let birth = $(`<td>${person.birth_year}</td>`)
+    let name = $(`<td>${students.name}</td>`)
+    let gender = $(`<td>${students.gender}</td>`)
+    let house = $(`<td>${students.house}</td>`)
     
     name.css('color','red')
     
-    row.append(name)
-    row.append(height)
-    row.append(birth)
+    row.append(name, gender, house)
     
     return row
   }
 
-  $(`#planetsButton`).click(() => {
-    if(!loaded){
-      loaded = true
+  $(`#staffButton`).click(() => {
+    $('#tableBody').empty()
+    $('#frontOne').text('Name')
+    $('#frontTwo').text('Gender')
+    $('#frontThree').text('Actor')
+    if(!localStorage.getItem('storedStaff')){
       $.ajax({
        type: 'GET',
-        url: 'https://swapi.co/api/planets/'
+        url: 'http://hp-api.herokuapp.com/api/characters/staff'
      }).done((res) => { //res = keyword    change it to whatever
-      $('#frontOne').text('Name')
-      $('#frontTwo').text('Diameter')
-      $('#frontThree').text('Population')
 
-      let data = res.results
-        for(planet of data) {
-         // tableBody.append(planet)
-          $('#tableBody').append(createTableRowTwo(planet))
+        for(staff of res) {
+         // tableBody.append(staff)
+         storedStaff.push({name: staff.name, gender: staff.gender, staff: staff.actor})
+          $('#tableBody').append(createTableRowTwo(staff))
         }
+        localStorage.setItem('storedStaff', JSON.stringify(storedStaff))
       })
-    }
+    } else {
+      for(staff of storedStaff) {
+        $('#tableBody').append(createTableRowTwo(staff))
+      }}
   })
   
-  function createTableRowTwo(planet) {
+  function createTableRowTwo(staff) {
     let row = $(`<tr></tr>`)
-    let name = $(`<td>${planet.name}</td>`)
-    let diameter = $(`<td>${planet.diameter}</td>`)
-    let population = $(`<td>${planet.population}</td>`)
+    let name = $(`<td>${staff.name}</td>`)
+    let gender = $(`<td>${staff.gender}</td>`)
+    let actor = $(`<td>${staff.actor}</td>`)
     
-    row.append(name)
-    row.append(diameter)
-    row.append(population)
-    
-    return row
-  }
-
-  $(`#shipsButton`).click(() => {
-    if(!loaded){
-      loaded = true
-      $.ajax({
-       type: 'GET',
-        url: 'https://swapi.co/api/starships/'
-     }).done((res) => { //res = keyword    change it to whatever
-      $('#frontOne').text('Name')
-      $('#frontTwo').text('Length')
-      $('#frontThree').text('Crew')
-      
-      let data = res.results
-        for(ships of data) {
-         // tableBody.append(ships)
-          $('#tableBody').append(createTableRowThree(ships))
-        }
-      })
-    }
-  })
-  
-  function createTableRowThree(ships) {
-    let row = $(`<tr></tr>`)
-    let name = $(`<td>${ships.name}</td>`)
-    let length = $(`<td>${ships.length}</td>`)
-    let crew = $(`<td>${ships.crew}</td>`)
-    
-    row.append(name)
-    row.append(length)
-    row.append(crew)
+    row.append(name, gender, actor)
     
     return row
   }
   
   $('#clearButton').click(() => {
     $('#tableBody').empty()
-    $('#frontOne').empty()
-    $('#frontTwo').empty()
-    $('#frontThree').empty()
+    $('#frontOne').text('Choose')
+    $('#frontTwo').text('One')
+    $('#frontThree').text('Please')
     loaded = false
   })
 
